f8cc355bc5c059cd4a3866addabec2ab4a9b9a13
sagardhiman01 <sagarsagar46605@gmail.com>
2026-05-17 22:09:28 +0530

    Update version.php to track commit aff93a0

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: text/plain');
-echo "Commit: 7b9a552bf771fb45a49806f357fcd5b8ef6c3b65\n";
-echo "Date: Sun May 17 21:58:00 2026 +0530\n";
+echo "Commit: aff93a059530fe86e00e82c5f1fa68b20ff441f7\n";
+echo "Date: Sun May 17 22:09:00 2026 +0530\n";
 echo "Active: YES\n";
